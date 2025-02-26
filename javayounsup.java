1c1ef89a4e25c5a41e7e41b7aaefccc3d60a1e4e
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-02-26 16:12:10 +0900

    double

--- javayounsup.java
+++ javayounsup.java
@@ -25,3 +25,39 @@ public class Main {
         // 以下にコードを追加
     }
 }
+
+public class Main {
+    public static void main(String... args) {
+        Integer i = 5;
+        System.out.println(i);
+         i -= 1;
+        System.out.println(i);
+        // 以下にコードを追加
+        
+    }
+}
+public class Main {
+    public static void main(String... args) {
+        double tax = 0.1;
+        System.out.println(tax); 
+
+    }
+}
+
+//値が 0.1 である tax を定数で宣言して、出力してください。
+
+
+public class Main {
+    public static void main(String... args) {
+        final Integer money = 1000;
+        money = 2000;
+        System.out.println(money);
+    }
+}
+// Main.java:4: error: cannot assign a value to final variable money
+//         money = 2000;
+//         ^
+// 1 error
+
+
+
