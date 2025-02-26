23864081ef0c2cd8facc75443f4adf1f1e8a5b16
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-02-26 15:59:15 +0900

    java

--- javayounsup.java
+++ javayounsup.java
@@ -6,3 +6,22 @@ public class Main {
     }
 }
 
+public class Main {
+    public static void main(String... args) {
+        Integer x = 10;
+          Integer y = 20;
+
+        System.out.println(--x + 50);
+        System.out.println(y-- + 50);
+    }
+}
+//05:インクリメント・デクリメント演算子
+public class Main {
+    public static void main(String... args) {
+        Integer i = 5;
+        System.out.println(i);
+        i +=1;
+        System.out.println(i);
+        // 以下にコードを追加
+    }
+}
