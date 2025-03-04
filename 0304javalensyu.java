d534b66161bc1dc2b40496a47b02135d507aa8d6
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-04 12:43:44 +0900

    textjava

--- 0304javalensyu.java
+++ 0304javalensyu.java
@@ -53,3 +53,19 @@ int num = 5;
 System.out.println(num++); // 5
 System.out.println(num); // 6
 System.out.println(++num); // 7
+
+
+
+Java
+class Sample1 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 % number2);
+    }
+}
