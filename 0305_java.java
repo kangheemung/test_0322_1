1690f9799ce205d4182bd7c2accd9f08ea32f6d9
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-05 17:04:54 +0900

    add STring

--- 0305_java.java
+++ 0305_java.java
@@ -395,3 +395,21 @@ public class Main {
     }
 
 }
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+       String a = sc.next();
+        String b = sc.next();
+        if (a.equals(b)){
+            System.out.println("OK");
+        }else{
+            System.out.println("NG");
+        }
+    }
+}
