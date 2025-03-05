2f817a619e87542ab955c3de231c5d14be50f180
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-05 13:54:53 +0900

    add

--- 0305_java.java
+++ 0305_java.java
@@ -11,3 +11,198 @@ public class Main {
         System.out.println(total);
     }
 }
+
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+         int n = Integer.parseInt(sc.nextLine());
+         int m = Integer.parseInt(sc.nextLine());
+             int total=n-m;
+        System.out.println(total);
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+  
+        System.out.println(8);
+        System.out.println(1);
+        System.out.println(3);
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 1231;
+        int num2 = 5178;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 437326;
+        int num2 = 9085;
+        //A を B で割った商
+// A を B で割った余り
+        int sum = num1 / num2;
+        int sum1 = num1 % num2;
+        System.out.println(sum+" "+ sum1);
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+          int num1 = 202, num2 = 134, num3 = 107;
+        int sum = (num1 + num2);
+        int total = sum * num3 * sum * num3;
+
+        System.out.println(total);
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+int N = 0;
+        N += 3286;
+        N *= 4736;
+        N %= 12312;
+
+       System.out.println(N);
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+    }
+}
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        
+        System.out.println(s);
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+         String s = sc.next();
+          String t = sc.next();
+        System.out.println(s);
+                System.out.println(t);
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+    }
+}
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s_1 = sc.next();
+        String s_2 = sc.next();
+
+        System.out.println(s_1);
+        System.out.println(s_2);
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        String T = sc.next();
+        System.out.println(S);
+         System.out.println(T);
+    
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+         Scanner sc = new Scanner(System.in);
+ int S = sc.nextInt();
+        int T = sc.nextInt();
+        int D = S - T;
+        int P = S * T;
+  
+        System.out.println(D+" "+P);
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+    int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int N = 0;
+        N += A;
+        N *= B;
+        N %= C;
+        System.out.println(N);
+    }
+}
