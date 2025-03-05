30bab2f2d669af223f924ab72d4d4942a5311480
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-05 11:49:04 +0900

    first commit

---
+++ 0305_java.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int m = Integer.parseInt(sc.nextLine());
+        int total = n * m;
+     
+        System.out.println(total);
+    }
+}
