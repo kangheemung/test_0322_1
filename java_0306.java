5488bc1778b2881f70edfdcbd0ac9a1bc098195f
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-06 16:08:34 +0900

    new

---
+++ java_0306.java
@@ -0,0 +1,301 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String  s = sc.nextLine();
+        String t= sc.nextLine();
+        System.out.println(s);
+        System.out.println(t);
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        String u = sc.nextLine();
+        System.out.println(s);
+        System.out.println(t);
+        System.out.println(u);
+    }
+}
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+
+        Scanner sc = new Scanner(System.in);
+        String[] strings = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            strings[i] = sc.nextLine();
+        }
+
+
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+}
+
+///同じ
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+         for (int i = 0; i < 1000; i++) {
+        String line = sc.nextLine();
+         System.out.println(line);
+         }
+    }
+
+    
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+
+            String str = "Hello paiza";
+        String[] parts = str.split(" ");
+       for (String part : parts) {
+            System.out.println(part);
+        }
+    }
+}
+
+public class SplitExample {
+    public static void main(String[] args) {
+        // カンマで区切られた文字列を分割
+        String str1 = "apple,banana,orange";
+        String[] fruits1 = str1.split(",");
+        for (String fruit : fruits1) {
+            System.out.println(fruit);
+        }
+
+        // スペースで区切られた文字列を分割
+        String str2 = "I love Java programming";
+        String[] words = str2.split(" ");
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = "He likes paiza";
+        String[] parts = line.split(" ");
+        for (String part : parts) {
+            System.out.println(part);
+        }
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = "one two three four five";
+        String[] parts = line.split(" ");
+        for (String part : parts) {
+            System.out.println(part);
+        }
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+         String[] parts = line.split(" ");
+         for (String part : parts) {
+            System.out.println(part);
+        }
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+    }
+}
+// javascript
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Enter a string: ', (line) => {
+//     const parts = line.split(" ");
+//     parts.forEach(part => {
+//         console.log(part);
+//     });
+
+//     rl.close();
+// });
+// //Ruby
+// line = gets.chomp
+// parts = line.split(" ")
+// parts.each do |part|
+//     puts part
+// end
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            int num = sc.nextInt();
+            System.out.println(num);
+        }
+
+    }
+}
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 100; i++) {
+            int num = sc.nextInt();
+            System.out.println(num);
+        }
+
+    }
+}
+
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            System.out.println(num);
+        }
+
+    }
+}
+
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+         for (int i = 0; i < N; i++) {
+            System.out.println(sc.nextInt());
+        }
+    }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            System.out.println(sc.nextInt());
+        }
+
+        sc.close();
+    }
+}
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            System.out.println(sc.nextInt());
+        }
+    }
+}
