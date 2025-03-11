056128520f157ecec9e1cc4bde634aa945cca065
kurotamashi30 <kurotamashi@hotmail.co.kr>
2025-03-11 17:54:10 +0900

    sljava

---
+++ java_0311.java
@@ -0,0 +1,12 @@
+class Sample1 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 % number2);
+    }
+}
